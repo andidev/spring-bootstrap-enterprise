37baff0b6e06e95f8f8f4242124c7b4a3831e050
Anders Steiner <anders.steiner@gmail.com>
2013-01-04 11:53:42 +0100

    Removed unused imports

--- core/src/main/java/org/andidev/config/appconfig/MySqlDatabaseConfig.java
+++ core/src/main/java/org/andidev/config/appconfig/MySqlDatabaseConfig.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.OpenJpaVendorAdapter;
 
 @Configuration
 @Profile("prod")
