91113e92ca3ef1580ffacd6fb2420f6f87a644bd
Anders Steiner <anders.steiner@gmail.com>
2013-09-26 21:23:37 +0200

    testing

--- src/main/java/org/andidev/applicationname/controller/LoginController.java
+++ src/main/java/org/andidev/applicationname/controller/LoginController.java
@@ -1,17 +1,39 @@
 package org.andidev.applicationname.controller;
 
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import org.andidev.applicationname.controller.AbstractApplicationController;
+import org.andidev.applicationname.entity.Opinion;
+import org.andidev.applicationname.repository.OpinionRepository;
+import org.andidev.applicationname.service.OpinionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
+@Slf4j
 public class LoginController extends AbstractApplicationController  {
     
+    @Inject
+    OpinionService opinionService;
+    @Inject
+    OpinionRepository opinionRepository;
+    
     @RequestMapping("/login")
+    @Transactional
     public String login(Model model, @RequestParam(required = false) String message) {
+        log.info("Testing!!!");
+        Opinion opinion = new Opinion("Test", "Lalala");
+        log.info(opinion.toString());
+        opinionService.create(opinion);
+        log.info(opinion.toString());
+        log.info("count = " + opinionRepository.count());
+        opinionRepository.findAll();
+        log.info("count = " + opinionRepository.findAll().toString());
+        
         model.addAttribute("message", message);
         return "login/login";
     }
