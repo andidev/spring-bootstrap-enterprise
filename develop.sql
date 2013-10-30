91956bd51908a536b38ad14e0629015159f76c09
Anders Steiner <anders.steiner@gmail.com>
2013-10-30 22:19:20 +0100

    Added sqls used for developing

---
+++ develop.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA `applicationname` ;
+CREATE SCHEMA `applicationname` ;
+
+SELECT * FROM applicationname.RevEntity;
+SELECT * FROM applicationname.Opinion_AUD;
+SELECT * FROM applicationname.Opinion;
+SELECT * FROM applicationname.OpinionTag_AUD;
+SELECT * FROM applicationname.OpinionTag;
+
+
+SELECT * FROM applicationname.User;
+SELECT * FROM applicationname.User_AUD;
+SELECT * FROM applicationname.User_PreferenceValue;
+SELECT * FROM applicationname.User_PreferenceValue_AUD;
