4c6e6a01a7c5eaf3c8de99ae5e9fc4a8d6162727
Anders Steiner <anders.steiner@gmail.com>
2013-10-31 22:29:54 +0100

    Removed unused code

--- src/main/java/org/andidev/applicationname/config/springsecurity/UserDetailsServiceImpl.java
+++ src/main/java/org/andidev/applicationname/config/springsecurity/UserDetailsServiceImpl.java
@@ -1,15 +1,9 @@
 package org.andidev.applicationname.config.springsecurity;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Set;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.andidev.applicationname.entity.User;
-import org.andidev.applicationname.entity.enums.Role;
 import org.andidev.applicationname.repository.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -32,14 +26,5 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("User with username = \"" + username + "\" was not found");
         }
         return user;
-//            User user = userRepository.findByUsername(username);
-//            return new org.springframework.security.core.userdetails.User(
-//                    user.getUsername(),
-//                    user.getPassword(),
-//                    user.isEnabled(),
-//                    user.isAccountNonExpired(),
-//                    user.isCredentialsNonExpired(),
-//                    user.isAccountNonLocked(),
-//                    user.getGrantedAuthorities());
     }
 }
