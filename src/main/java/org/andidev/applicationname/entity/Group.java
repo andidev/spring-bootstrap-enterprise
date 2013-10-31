533c010f821f96f93a5e3f21bbb417b7ecc60e1a
Anders Steiner <anders.steiner@gmail.com>
2013-10-31 22:59:48 +0100

    Removed unused code

--- src/main/java/org/andidev/applicationname/entity/Group.java
+++ src/main/java/org/andidev/applicationname/entity/Group.java
@@ -16,13 +16,11 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.andidev.applicationname.entity.enums.Role;
-import org.hibernate.envers.Audited;
 import static javax.persistence.CascadeType.*;
 import static javax.persistence.EnumType.*;
 import static javax.persistence.FetchType.*;
 import static lombok.AccessLevel.*;
 import org.andidev.applicationname.entity.abstracts.IdEntity;
-import static org.hibernate.envers.RelationTargetAuditMode.NOT_AUDITED;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
