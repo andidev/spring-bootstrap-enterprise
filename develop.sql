c5fe4bf3950e0d39378264237fd8087d0b0286af
Anders Steiner <anders.steiner@gmail.com>
2013-11-23 17:18:17 +0100

    Updated developer.sql

--- develop.sql
+++ develop.sql
@@ -8,6 +8,7 @@ SELECT * FROM applicationname.OpinionTag_AUD;
 SELECT * FROM applicationname.OpinionTag;
 
 
+SELECT * FROM applicationname.Group;
 SELECT * FROM applicationname.User;
 SELECT * FROM applicationname.User_AUD;
 SELECT * FROM applicationname.User_PreferenceValue;
