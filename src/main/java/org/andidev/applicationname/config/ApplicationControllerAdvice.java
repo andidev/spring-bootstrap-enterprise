d56a81af6a7bb3a83f64dd0cdd4eea094e647a21
Anders Steiner <anders.steiner@gmail.com>
2013-11-27 21:47:00 +0100

    Added exceptionhandler

--- src/main/java/org/andidev/applicationname/config/ApplicationControllerAdvice.java
+++ src/main/java/org/andidev/applicationname/config/ApplicationControllerAdvice.java
@@ -3,8 +3,12 @@ package org.andidev.applicationname.config;
 import javax.inject.Inject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
@@ -33,4 +37,17 @@ public class ApplicationControllerAdvice {
     public String getApplicationEnvironment() {
         return applicationEnvironment;
     }
+
+//    @ExceptionHandler(Throwable.class)
+//    public ModelAndView handleAllException(Exception ex) {
+//
+//        ModelAndView model = new ModelAndView("pages/error");
+//        model.addObject("errMsg", "this is Exception.class");
+//
+//        return model;
+//    }
+//    @InitBinder
+//    public void initBinder(WebDataBinder binder) {
+//        binder.
+//    }
 }
