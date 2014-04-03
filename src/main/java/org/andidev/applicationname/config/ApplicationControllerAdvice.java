ea035193dac4305bae05217eb5685e0a71c42b2f
Anders Steiner <anders.steiner@gmail.com>
2014-04-03 13:20:17 +0200

    Added ApplicationEnvironmentUtils methods as model attributes to ApplicationControllerAdvice

--- src/main/java/org/andidev/applicationname/config/ApplicationControllerAdvice.java
+++ src/main/java/org/andidev/applicationname/config/ApplicationControllerAdvice.java
@@ -1,12 +1,13 @@
 package org.andidev.applicationname.config;
 
+import org.andidev.applicationname.util.ApplicationEnvironmentUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
-    
+
     @Value("${application.name}")
     private String applicationName;
     @Value("${application.version}")
@@ -27,5 +28,25 @@ public class ApplicationControllerAdvice {
     @ModelAttribute("applicationEnvironment")
     public String getApplicationEnvironment() {
         return applicationEnvironment;
+    }
+
+    @ModelAttribute("isLocalhostEnvironment")
+    public boolean isLocalhostEnvironment() {
+        return ApplicationEnvironmentUtils.isLocalhostEnvironment();
+    }
+
+    @ModelAttribute("isDevelopmentEnvironment")
+    public boolean isDevelopmentEnvironment() {
+        return ApplicationEnvironmentUtils.isDevelopmentEnvironment();
+    }
+
+    @ModelAttribute("isTestEnvironment")
+    public boolean isTestEnvironment() {
+        return ApplicationEnvironmentUtils.isTestEnvironment();
+    }
+
+    @ModelAttribute("isProductionEnvironment")
+    public boolean isProductionEnvironment() {
+        return ApplicationEnvironmentUtils.isProductionEnvironment();
     }
 }
