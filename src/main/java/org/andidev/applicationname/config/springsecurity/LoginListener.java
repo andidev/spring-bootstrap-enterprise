5af7a94231faf264b074fad23dac382a2e92f849
Anders Steiner <anders.steiner@gmail.com>
2014-04-06 15:38:04 +0200

    Updated LoginListener to also listen to Switch User Logins

--- src/main/java/org/andidev/applicationname/config/springsecurity/LoginListener.java
+++ src/main/java/org/andidev/applicationname/config/springsecurity/LoginListener.java
@@ -1,33 +1,56 @@
 package org.andidev.applicationname.config.springsecurity;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.andidev.applicationname.config.logging.MDC;
 import org.andidev.applicationname.entity.User;
-import static org.andidev.applicationname.util.ApplicationUtils.*;
+import static org.andidev.applicationname.util.ApplicationUtils.getSession;
+import static org.andidev.applicationname.util.ApplicationUtils.getUser;
+import static org.andidev.applicationname.util.ApplicationUtils.getUsername;
 import static org.andidev.applicationname.util.TimeUtils.format;
+import org.joda.time.Period;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.switchuser.AuthenticationSwitchUserEvent;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class LoginListener implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
+public class LoginListener {
 
-    @Inject
-    HttpSession session;
+    @Component
+    static class FormLoginAndRememberMeLoginListener implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
 
-    @Override
-    public void onApplicationEvent(InteractiveAuthenticationSuccessEvent event) {
-        MDC.putSession(getSessionId());
-        User user = getUser();
-        setAutomaticLogoutTime(session, user);
+        @Inject
+        LoginListener listener;
+
+        @Override
+        public void onApplicationEvent(InteractiveAuthenticationSuccessEvent event) {
+            listener.onLogin(getUser());
+        }
     }
 
-    private void setAutomaticLogoutTime(HttpSession session, User user) {
-        log.info("Setting the automatic logout time for user {} to {}", user.getUsername(), format(user.getAutomaticLogoutTime()));
-        int automaticLogoutTimeInSeconds = user.getAutomaticLogoutTime().toStandardSeconds().getSeconds() ;
-        session.setMaxInactiveInterval(automaticLogoutTimeInSeconds);
+    @Component
+    static class SwitchUserLoginListener implements ApplicationListener<AuthenticationSwitchUserEvent> {
+
+        @Inject
+        LoginListener listener;
+
+        @Override
+        public void onApplicationEvent(AuthenticationSwitchUserEvent event) {
+            listener.onLogin((User) event.getTargetUser());
+        }
     }
+
+    private void onLogin(User user) {
+        MDC.putUsername(user.getUsername());
+        setAutomaticLogoutTime(user.getAutomaticLogoutTime());
+    }
+
+    private void setAutomaticLogoutTime(Period automaticLogoutTime) {
+        log.info("Setting automatic logout time to {}", format(automaticLogoutTime));
+        int automaticLogoutTimeInSeconds = automaticLogoutTime.toStandardSeconds().getSeconds();
+        getSession().setMaxInactiveInterval(automaticLogoutTimeInSeconds);
+    }
+
 }
