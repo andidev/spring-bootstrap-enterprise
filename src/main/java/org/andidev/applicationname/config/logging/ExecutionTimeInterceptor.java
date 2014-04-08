87ebe39d021c2f95eb79058e1c3f24ecadf2e716
Anders Steiner <anders.steiner@gmail.com>
2014-04-08 21:49:45 +0200

    Skipping the calculation of execution time if the request is redirected

--- src/main/java/org/andidev/applicationname/config/logging/ExecutionTimeInterceptor.java
+++ src/main/java/org/andidev/applicationname/config/logging/ExecutionTimeInterceptor.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import static org.andidev.applicationname.util.TimeUtils.formatMilliseconds;
+import static org.apache.commons.lang3.StringUtils.startsWith;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -22,6 +23,10 @@ public class ExecutionTimeInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (isRedirect(modelAndView)) {
+            // Skip calulation of execution time if the view is redirected to another url
+            return;
+        }
 
         // Calculate execution time
         long startTime = (Long) request.getAttribute("startTime");
@@ -34,6 +39,10 @@ public class ExecutionTimeInterceptor extends HandlerInterceptorAdapter {
         if (log.isTraceEnabled()) {
             log.trace("executionTime = {}", executionTime);
         }
+    }
+
+    private boolean isRedirect(ModelAndView modelAndView) {
+        return startsWith(modelAndView.getViewName(), "redirect:");
     }
 
 }
