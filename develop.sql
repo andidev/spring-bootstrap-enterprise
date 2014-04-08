ae0cb9b77415f477421acc5dc0e2171e0beb17a1
Anders Steiner <anders.steiner@gmail.com>
2014-04-08 21:45:43 +0200

    Single-quoted sqls

--- develop.sql
+++ develop.sql
@@ -1,15 +1,15 @@
 DROP SCHEMA `application-name` ;
 CREATE SCHEMA `application-name` ;
 
-SELECT * FROM applicationname.RevEntity;
-SELECT * FROM applicationname.Opinion_AUD;
-SELECT * FROM applicationname.Opinion;
-SELECT * FROM applicationname.OpinionTag_AUD;
-SELECT * FROM applicationname.OpinionTag;
+SELECT * FROM `application-name.RevEntity`;
+SELECT * FROM `application-name.Opinion_AUD`;
+SELECT * FROM `application-name.Opinion`;
+SELECT * FROM `application-name.OpinionTag_AUD`;
+SELECT * FROM `application-name.OpinionTag`;
 
 
-SELECT * FROM applicationname.Group;
-SELECT * FROM applicationname.User;
-SELECT * FROM applicationname.User_AUD;
-SELECT * FROM applicationname.User_PreferenceValue;
-SELECT * FROM applicationname.User_PreferenceValue_AUD;
+SELECT * FROM `application-name.Group`;
+SELECT * FROM `application-name.User`;
+SELECT * FROM `application-name.User_AUD`;
+SELECT * FROM `application-name.User_PreferenceValue`;
+SELECT * FROM `application-name.User_PreferenceValue_AUD`;
